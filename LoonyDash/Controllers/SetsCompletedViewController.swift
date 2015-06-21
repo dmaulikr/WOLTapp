1dc9937602000e10cb6d9f6405012f665ea5bb9b
Ming <mingyeow@gmail.com>
2015-06-21 19:28:16 +0800

    added storyboards and viewcontrollers

--- LoonyDash/Controllers/SetsCompletedViewController.swift
+++ LoonyDash/Controllers/SetsCompletedViewController.swift
@@ -1,33 +1,27 @@
 //
-//  UserProfileViewController.swift
+//  SetsCompletedViewController.swift
 //  LoonyDash
 //
-//  Created by ming yeow ng on 6/16/15.
-//  Copyright (c) 2015 LooneyDash. All rights reserved.
+//  Created by ming yeow ng on 6/21/15.
+//  Copyright © 2015 LooneyDash. All rights reserved.
 //
 
 import UIKit
 
-class UserProfileViewController: UIViewController {
-//class UserProfileViewController: UIPageViewController, UIPageViewControllerDataSource  {
+class SetsCompletedViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Profile"
-        DashClient.sharedInstance.fetchWorkoutSetsForUser { (workouts: [WorkoutSet]!, error: NSError!) -> Void in
-            print(workouts)
-        }
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.title = "Profile"
-    }
     
+
     /*
     // MARK: - Navigation
 
--- LoonyDash/UIStoryboard.swift
+++ LoonyDash/UIStoryboard.swift
@@ -11,12 +11,23 @@ import Foundation
 extension UIStoryboard {
     class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()) }
     class func setsStoryboard() -> UIStoryboard { return UIStoryboard(name: "Sets", bundle: NSBundle.mainBundle()) }
+    class func profileStoryboard() -> UIStoryboard { return UIStoryboard(name: "Profile", bundle: NSBundle.mainBundle()) }
     
     
     class func loginViewController() -> LoginViewController? {
         return mainStoryboard().instantiateViewControllerWithIdentifier("LoginViewController") as? LoginViewController
     }
 
+    class func personalRecordsViewController() -> PersonalRecordsViewController? {
+        return profileStoryboard().instantiateViewControllerWithIdentifier("PersonalRecordsViewController") as? PersonalRecordsViewController
+    }
+
+    class func setsCompletedViewController() -> SetsCompletedViewController? {
+        return profileStoryboard().instantiateViewControllerWithIdentifier("SetsCompletedViewController") as? SetsCompletedViewController
+    }
+
+
+    
     
 //    class func repsAndWeightViewController() -> RepsAndWeightVC {
 //        return setsStoryboard().instantiateViewControllerWithIdentifier("RepsAndWeightVC") as! RepsAndWeightVC
